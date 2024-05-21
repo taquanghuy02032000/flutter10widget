74a0f77ab0f7790fdc5e3b76de0c44f9c00c7b02
taquanghuy02032000 <taquanghuy0386356200@gmail.com>
2024-05-21 19:51:21 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: FirstScreen(),
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("First project"),
+      ),
+      body: Container(
+        color: Colors.white,
+        child: const Text('Demo container'),
+      ),
+    );
+  }
+}
